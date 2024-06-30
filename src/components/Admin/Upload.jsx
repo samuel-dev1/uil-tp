7407425108fb843ab74d9856547fdc87e54e7277
samuel stephen opeyemi <samuelyyyy257@gmail.com>
2024-06-30 11:16:17 +0100

    uploading students now working

--- src/components/Admin/Upload.jsx
+++ src/components/Admin/Upload.jsx
@@ -1,19 +1,28 @@
 
 
+
+const token = JSON.parse(localStorage.getItem('token'));
+
+
+const uploadUrl = `https://uil-tp.com.ng/auth/upload-school?token=${encodeURIComponent(token)}`;
+
+
 export const Upload = () => {
+
+   const token = JSON.parse(localStorage.getItem('token')); 
   return (
     <div className="w-full py-10 px-12 h-auto">
-        <a href="facebook.com">
+        <a href={uploadUrl}>
         <button className='mx-2 py-3 px-5 rounded-xl bg-background1'>
            Upload school
         </button>
         </a>
-        <a href="facebook.com">
+        <a href="https://uil-tp.com.ng/auth/upload-supervisor">
         <button className='mx-2 py-3 px-5 rounded-xl bg-background1'>
            Upload Lecturer
         </button>
         </a>
-        <a href="facebook.com">
+        <a href="https://uil-tp.com.ng/auth/uploads">
         <button className='mx-2 py-3 px-5 rounded-xl bg-background1'>
            Upload Student
         </button>
